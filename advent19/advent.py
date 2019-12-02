72c26aea38f46a133e9e895938e9450e5e417012
unknown <berzinsu@gmail.com>
2019-12-02 07:59:08 +0200

    Day 2

--- advent19/advent.py
+++ advent19/advent.py
@@ -31,3 +31,50 @@ def day_1_p2():
     weights = f.read().splitlines()
     fuels = map(calculateFuelRecursive, weights)
     return sum(fuels)
+
+# -------------- DAY 2---------------------
+
+def do_op(intcode, pos):
+    op = intcode[pos]
+    pos_1 = intcode[pos+1]
+    pos_2 = intcode[pos+2]
+    result_pos = intcode[pos+3]
+    if (op == 1):
+        intcode[result_pos] = intcode[pos_1]+intcode[pos_2]
+    else:
+        intcode[result_pos] = intcode[pos_1]*intcode[pos_2]
+    return intcode
+
+def do_seq(intcode):
+    seqs = intcode[::4]
+
+    for x in range(len(seqs)):
+        if (intcode[x*4] == 99):
+           break
+        intcode = do_op(intcode,x*4)
+    return intcode
+
+def day_2():
+    with open(InputPath('inputs/input_2.txt')) as f:
+        intcode = map(int, f.read().strip().split(','))
+        intcode[1]=12
+        intcode[2]=2
+        do_seq(intcode)
+        return intcode[0]
+
+def day_2_p2():
+    with open(InputPath('inputs/input_2.txt')) as f:
+        intcode = map(int, f.read().strip().split(','))
+        
+        for x in range(99):
+            for y in range(99):
+                seq = list(intcode)
+                seq[1]=x
+                seq[2]=y
+                result = do_seq(seq)[0]
+                if(result == 19690720):
+                    noun =x
+                    verb=y
+        return 100*noun+verb
+
+print day_2_p2()
--- advent19/test_advent.py
+++ advent19/test_advent.py
@@ -28,3 +28,23 @@ def test_fuel_rec_100756():
 
 def test_day_1_p2():
     assert adv.day_1_p2() == 4896582
+
+# -------------- DAY 2---------------------
+
+def test_do_op_1():
+    assert adv.do_op([1,0,0,0,99],0)[0] == 2
+
+def test_do_op_2():
+    assert adv.do_op([2,3,0,3,99],0)[3] == 6
+
+def test_do_op_3():
+    assert adv.do_op([2,4,4,5,99,0],0)[5] == 9801
+
+def test_do_op_4():
+    assert adv.do_seq([1,1,1,4,99,5,6,0,99])[0] == 30
+
+def test_day_2():
+    assert adv.day_2() == 3058646
+
+def test_day_2_p2():
+    assert adv.day_2_p2() == 8976
